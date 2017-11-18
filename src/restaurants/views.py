740a5836c2bbc1c9d1d5716adab6e2541d62f875
Louis <louisaleksieb.dagusen@gmail.com>
2017-11-18 13:36:59 +0800

    created a function based view

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# function based view
+def home(request):
+	return HttpResponse("Hello")
+	# return render(request, "home.html",{})
