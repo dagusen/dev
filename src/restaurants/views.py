34cd9c2c0c06c0302f95396986dec4ccca5f2ee0
Louis <louisaleksieb.dagusen@gmail.com>
2017-11-19 16:40:07 +0800

    Template View

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,37 +2,26 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import TemplateView
+
 # Create your views here.
 
-# function based view
-def home(request):
-	num = None
-	some_list = [
-		random.randint(0,100000000),
-		random.randint(0,100000000),
-		random.randint(0,100000000)
-	]
-	condition_bool_item = True
-	if condition_bool_item:
-		num = random.randint(0,100000000)
-	context = {
-		"num":num,
-		"some_list":some_list
-	}
-	return render(request, "home.html", context)
+class HomeView(TemplateView):
+	template_name = 'home.html'
 
-def about(request):
-	
-	return render(request, "about.html",)
-
-def contact(request):
-	
-	return render(request, "contact.html",)
-
-class ContactView(View):
-	def get(self, request, *args, **kwargs):
-		print(kwargs)
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		num = None
+		some_list = [
+			random.randint(0,100000000),
+			random.randint(0,100000000),
+			random.randint(0,100000000)
+		]
+		condition_bool_item = True
+		if condition_bool_item:
+			num = random.randint(0,100000000)
 		context = {
-
+			"num":num,
+			"some_list":some_list
 		}
-		return render(request, "contact.html", context)
+		return context
